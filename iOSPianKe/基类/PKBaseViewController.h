2a148ec2d3e5540229c597f50a597264d95984e5
wsy516200 <chinac7@vip.qq.com>
2018-05-23 19:01:36 +0800

    Update PKBaseViewController.h

--- iOSPianKe/基类/PKBaseViewController.h
+++ iOSPianKe/基类/PKBaseViewController.h
@@ -2,8 +2,8 @@
 //  PKBaseViewController.h
 //  iOSPianKe
 //
-//  Created by 赵金鹏 on 16/1/14.
-//  Copyright © 2016年 赵金鹏. All rights reserved.
+//  Created by 吴思远 on 16/1/14.
+//  Copyright © 2016年 吴思远. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
